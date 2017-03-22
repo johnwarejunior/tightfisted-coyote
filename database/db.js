523b6c0f68ec5e18c7404a13e3361ace58b1f8d2
Helen Yau <helenyau13@gmail.com>
2017-03-22 09:03:23 -0700

    implement strikethrough and option to add items to the list

--- database/db.js
+++ database/db.js
@@ -1,14 +1,26 @@
-const {db} = require('./connection.js')
+const { db } = require('./connection.js')
 
 const getAllItems = () => {
   return db.any('SELECT * FROM todo')
 }
 
 const addItems = (task) => {
-  return db.none('INSERT INTO todo(task) VALUES($1)', task)
+  const psql = `INSERT INTO todo(task) VALUES($1)`
+  return db.none(psql, [task])
+}
+
+const completeItems = (id) => {
+  const psql = `UPDATE todo SET completed  = NOT completed WHERE id=$1`
+  return db.none(psql, [id])
+}
+
+const deleteItems = (id) => {
+  const psql = `DELETE FROM todo WHERE id=$1`
+  return db.none(psql, [id])
 }
 
 module.exports = {
   getAllItems,
-  addItems
+  addItems,
+  completeItems
 }
